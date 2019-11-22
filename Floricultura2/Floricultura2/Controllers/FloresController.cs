5da6baf99c13d1263e3a77339dc328b63cf66d1d
FIC <FIC@info.local>
2019-11-21 22:41:25 -0200

    flores view

---
+++ Floricultura2/Floricultura2/Controllers/FloresController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Floricultura2.Models;
+using Floricultura2.Repositorio;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Floricultura2.Controllers
+{
+    public class FloresController : Controller
+    {
+        private FlorRepositorio _repositorio;
+
+        public FloresController(List<Flor> flores)
+        {
+            _repositorio = new FlorRepositorio(flores);
+        }
+        public IActionResult Index()
+        {
+            var flores = _repositorio.Listar();
+            return View(flores);
+        }
+
+
+        //fazendo com que a nossa florcontroller nos retorne uma view
+        ///sem este datanotation nada funciona 
+        [HttpGet]
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(Flor flor)
+        {
+            _repositorio.Adicionar(flor);
+            return View();
+        }
+    }
+}
---
+++ Floricultura2/Floricultura2/Models/Flor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Floricultura2.Models
+{
+    public class Flor
+    {
+        public int Id { get; set; }
+
+        public string Especie { get; set; }
+
+        public string Descricao { get; set; }
+
+        public string Cor { get; set; }
+
+        public double Preco { get; set; }
+
+
+    }
+}
